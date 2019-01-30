8501ba157759bca3a0bde9fc548ae0027d603c47
Khaled Mohamed <khaled.mohamed@outlook.com>
2019-01-30 13:30:35 +0200

    Adding District to Setup

---
+++ PIE_Setup/models/pie_setup_district.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api ,fields, models
+
+
+class district(models.Model):
+    _name = "pie.setup.district"
+
+    name = fields.Char(string="District Name",size=150,required=True)
+    area_id = fields.Many2one('pie.setup.area' , string='Parent Area',required=True)
+    region_id = fields.Many2one(related='area_id.region_id' ,string='Governorate Region',required=True)
+    country_id=fields.Many2one(related='region_id.country_id',required=True)
+    active = fields.Boolean('Active', default=True)
+
+
+     
+
+    @api.constrains('name')
+    def _check_duplicate_code(self):
+        names = self.search([])
+        for c in names:
+            if self.name.lower() == c.name.lower() and self.id != c.id:
+                raise exceptions.ValidationError("Error: Name must be unique")
